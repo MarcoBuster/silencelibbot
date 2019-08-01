1c988bf207d50a1d099592adfe532ef970ab56b5
casungo <a.casnigo@gmail.com>
2019-08-01 23:19:47 +0200

    Update bot.py
    folders for constants

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 from PIL import Image, ImageDraw, ImageFont
-image = Image.open('crab.jpg')
+image = Image.open('preset/crab.jpg')
 draw = ImageDraw.Draw(image)
-font = ImageFont.truetype('Roboto-Regular.ttf', size=63)
+font = ImageFont.truetype('font/Roboto-Regular.ttf', size=63)
 (x, y) = (15, 81)
 message = "the quick brown fox jumps over the lazy dog"
 color = 'rgb(255, 255, 255)'
 draw.text((x, y), message, fill=color, font=font)
-image.save('test.jpg')
+image.save('test/test.jpg')
