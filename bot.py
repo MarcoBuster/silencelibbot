4acdbe98e69287b9e71c71c09d52c3bb055c8d2a
Marco Aceti <mail@marcoaceti.it>
2019-08-15 15:29:21 +0200

    Code enhancements

--- bot.py
+++ bot.py
@@ -1,24 +1,32 @@
-import botogram
+import os
+import random
 import config
+
+import botogram
 from PIL import Image, ImageDraw, ImageFont
 
 bot = botogram.create(config.BOT_TOKEN)
 
-@bot.process_message
-def process_messages(chat, message):
-    if not message.photo:
-        return
-
-    output/output.jpg = image = Image.open('template/crab.jpg')
+def _convert_image(text):
+    image = Image.open('template/crab.jpg')
     draw = ImageDraw.Draw(image)
     font = ImageFont.truetype('font/Roboto-Regular.ttf', size=63)
     (x, y) = (15, 81)
-    message = "message"
+    message = text
     color = 'rgb(255, 255, 255)'
     draw.text((x, y), message, fill=color, font=font)
-    image.save('output/output.jpg')(message.photo)
+    output_path = 'output/out_{n}.jpg'.format(n=random.randint(0, 1000000))
+    image.save(output_path)
+    return output_path
 
-    message.reply_with_photo(path=output/output.jpg)
+@bot.process_message
+def process_messages(chat, message):
+    if not message.text:
+        return
+
+    output_path = _convert_image(message.text)
+    message.reply_with_photo(path=output_path)
+    os.remove(output_path)
 
 if __name__ == '__main__':
     bot.run()
