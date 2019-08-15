63e1985462f4a0e09d72f83bb11a8634576afd6d
casungo <a.casnigo@gmail.com>
2019-08-15 15:06:41 +0200

    bot running, but not responding
    Co-Authored-By: Marco Aceti <mail@marcoaceti.it>

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # silencelibbot
  A telegram bot based on the "Silence, Liberal" meme
+
+You can see the future changes that I want to do to this bot here: https://trello.com/b/ansk7Oul/silencelibbot-roadmap
+
+[WIP]
--- bot.py
+++ bot.py
@@ -1,9 +1,24 @@
+import botogram
+import config
 from PIL import Image, ImageDraw, ImageFont
-image = Image.open('preset/crab.jpg')
-draw = ImageDraw.Draw(image)
-font = ImageFont.truetype('font/Roboto-Regular.ttf', size=63)
-(x, y) = (15, 81)
-message = "the quick brown fox jumps over the lazy dog"
-color = 'rgb(255, 255, 255)'
-draw.text((x, y), message, fill=color, font=font)
-image.save('test/test.jpg')
+
+bot = botogram.create(config.BOT_TOKEN)
+
+@bot.process_message
+def process_messages(chat, message):
+    if not message.photo:
+        return
+
+    output/output.jpg = image = Image.open('template/crab.jpg')
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype('font/Roboto-Regular.ttf', size=63)
+    (x, y) = (15, 81)
+    message = "message"
+    color = 'rgb(255, 255, 255)'
+    draw.text((x, y), message, fill=color, font=font)
+    image.save('output/output.jpg')(message.photo)
+
+    message.reply_with_photo(path=output/output.jpg)
+
+if __name__ == '__main__':
+    bot.run()
